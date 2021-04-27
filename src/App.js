636a05602ceae600a7d0e78a8487a6760f56eade
martinmags <martinremags@gmail.com>
2021-04-26 23:15:25 -0700

    removed warnings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme } from 'styles/Theme';
+import { darkTheme } from 'styles/Theme';
 import GlobalStyle from 'styles/GlobalStyles';
 import Heading from 'components/Heading';
 import Container from 'components/Container';
